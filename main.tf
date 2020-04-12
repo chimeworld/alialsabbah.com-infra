db934938fc0a79c95597c3cc5c8e81ed6aa97ec8
Ali <ali.alsabbah08@gmail.com>
2020-04-11 19:27:22 -0700

    Create public access block

--- main.tf
+++ main.tf
@@ -18,6 +18,15 @@ resource "aws_s3_bucket" "www-alialsabbah-site" {
     redirect_all_requests_to = "alialsabbah.com"
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "block-alialsabbah-site" {
+  bucket = aws_s3_bucket.alialsabbah-site.id
+  block_public_acls = false
+  block_public_policy = false
+  ignore_public_acls = false
+  restrict_public_buckets = false
+}
+
 resource "aws_s3_bucket" "alialsabbah-cdn" {
   bucket = "alialsabbah"
   acl    = "private"
