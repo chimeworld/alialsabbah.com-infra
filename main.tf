c1b65643f67d541791141aaa1576c102c9c66eef
Ali <ali.alsabbah08@gmail.com>
2020-04-11 19:26:10 -0700

    Create main bucket config

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
 provider "aws" {
   region = "us-west-1"
 }
+
+resource "aws_s3_bucket" "alialsabbah-site" {
+  bucket = "alialsabbah.com"
+  policy = file("policy.json")
+  acl    = "private"
+
+  website {
+    index_document = "index.html"
+  }
+}
