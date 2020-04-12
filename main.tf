ebf3674d63748d046922261a57dd8642e07eb2b5
Ali <ali.alsabbah08@gmail.com>
2020-04-11 19:26:32 -0700

    Create subdomain bucket config

--- main.tf
+++ main.tf
@@ -11,3 +11,18 @@ resource "aws_s3_bucket" "alialsabbah-site" {
     index_document = "index.html"
   }
 }
+
+resource "aws_s3_bucket" "www-alialsabbah-site" {
+  bucket = "www.alialsabbah.com"
+  website {
+    redirect_all_requests_to = "alialsabbah.com"
+  }
+}
+resource "aws_s3_bucket" "alialsabbah-cdn" {
+  bucket = "alialsabbah"
+  acl    = "private"
+
+  website {
+    index_document = "index.html"
+  }
+}
